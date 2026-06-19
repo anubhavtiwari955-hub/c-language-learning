fb2779233d29b006802883f70b7fd41dce1aabb8
anubhavtiwari955-hub <anubhavtiwari955@gmail.com>
2026-06-19 21:17:16 +0530

    Create decision.c

---
+++ chapter 3 - Control flow/decision.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int age = 44;
+    if(age>18){
+    printf("You can drive ");
+    }
+    else if(age>60){
+        printf("You can drive and you are senior citizen");
+    }
+    else("You cannot drive");
+    return 0;
+}
