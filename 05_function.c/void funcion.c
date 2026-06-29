cf8ba582dedfc548987a15ef11a8f849694af839
anubhavtiwari955-hub <anubhavtiwari955@gmail.com>
2026-06-29 21:19:45 +0530

    Implement functions for greeting messages

---
+++ 05_function.c/void funcion.c
@@ -0,0 +1,18 @@
+void goodmorning();
+void goodafternoon();
+void goodevening();
+     void goodmorning(){
+         printf("good morning\n");
+     }
+     void goodafternoon(){
+         printf("good afternoon\n");
+     }
+     void goodevening(){
+         printf("good evening\n");
+     }
+     int main(){
+         goodmorning();
+         goodafternoon();
+         goodevening();
+         return 0;
+     }
